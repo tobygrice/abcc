d8429c2793902953da6baff2192d03634355b048
tobygrice <tobygrice@icloud.com>
2026-04-29 16:22:49 +0930

    Started driver code - added clap interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about = "Another Bad C Compiler")]
+struct Cli {
+    /// Run the lexer. Stop before parsing
+    #[arg(long, conflicts_with_all = ["parse", "codegen", "emit_assembly"])]
+    lex: bool,
+
+    /// Run the lexer and parser. Stop before assembly generation
+    #[arg(long, conflicts_with_all = ["lex", "codegen", "emit_assembly"])]
+    parse: bool,
+
+    /// Perform lexing, parsing, and assembly generation. Stop before code emission
+    #[arg(long, conflicts_with_all = ["lex", "parse", "emit_assembly"])]
+    codegen: bool,
+
+    /// Emit an assembly file. Stop before assembling or linking
+    #[arg(short = 'S', conflicts_with_all = ["lex", "parse", "codegen"])]
+    emit_assembly: bool,
+
+    /// C source file to compile
+    input_file: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
